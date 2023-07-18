caa64e08fcfd0d0819b92df67a360833e2f4d6b5
Steerpike75 <39983881+Steerpike75@users.noreply.github.com>
2023-07-18 15:04:02 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,3 @@
+// Create Web Server
+// Create a web server that can listen to requests for /hello and responds with some HTML that says <h1>Hello!</h1>
+// For an extra challenge, make the homepage (localhost:8080) display a file index that links to both hello and goodbye and returns the corresponding HTML files.
